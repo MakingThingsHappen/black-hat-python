5f122462c6d1e5973849431b6ed6e2549575e30e
yanzhao <yanzhao@mbyte.tech>
2016-07-05 22:08:07 +0800

    tcp server

---
+++ chap02/tcp_server.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import socket
+import threading
+
+bind_ip = '0.0.0.0'
+bind_port = 9999
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# bind
+server.bind((bind_ip, bind_port))
+server.listen(5)
+
+
+def handle_client(client_socket):
+    request = client_socket.recv(1024)
+    print("[*] Received: {0}".format(request))
+    client_socket.send("ACK!")
+    client_socket.close()
+
+
+while True:
+    client, addr = server.accept()
+    threading.Thread(target=handle_client, args=(client,)).start()
